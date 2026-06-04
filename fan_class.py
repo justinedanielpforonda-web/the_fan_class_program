59f172c23a63c2370df125b7d6bd28dbe6aa6f97
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-06-04 12:46:55 +0800

    add constructor and initialize

--- fan_class.py
+++ fan_class.py
@@ -1,3 +1,13 @@
 class Fan:
-    def __init__(self):
-        
+
+    #three constants named SLOW, MEDIUM and FAST.
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+    
+    #constructor
+    def __init__(self, speed=SLOW, on=False, radius=5, color="blue"):
+        self.speed = speed
+        self.on = on
+        self.radius = radius
+        self.color = color
