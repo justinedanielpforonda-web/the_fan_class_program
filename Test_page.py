b7e64dd3c8237f28748ba5b5e37686cb638a59c3
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-06-04 22:38:10 +0800

    try  and catching and can display info

--- Test_page.py
+++ Test_page.py
@@ -1,18 +1,38 @@
 from fan_class import Fan
 
-# First Fan Object
-fan1 = Fan(
-    speed=Fan.FAST,
-    radius=10,
-    color="yellow",
-    on=True
-)
+try:
 
-# Second Fan Object
-fan2 = Fan(
-    speed=Fan.MEDIUM,
-    radius=5,
-    color="blue",
-    on=False
-)
+    # First Fan Object
+    fan1 = Fan(
+        speed=Fan.FAST,
+        radius=10,
+        color="yellow",
+        on=True
+    )
 
+    # Second Fan Object
+    fan2 = Fan(
+        speed=Fan.MEDIUM,
+        radius=5,
+        color="blue",
+        on=False
+    )
+
+    print("\nFIRST FAN")
+    fan1.display_info()
+
+    print("\nSECOND FAN")
+    fan2.display_info()
+
+    print("\nOBJECT REPRESENTATION")
+    print(fan1)
+    print(fan2)
+
+except ValueError as error:
+    print(f"Value Error: {error}")
+
+except TypeError as error:
+    print(f"Type Error: {error}")
+
+except Exception as error:
+    print(f"Unexpected Error: {error}")
