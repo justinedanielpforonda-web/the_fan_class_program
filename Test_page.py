a0d74606564657642567daafb65c377e7b2d7325
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-06-04 21:25:21 +0800

    two objects and importing libraries

---
+++ Test_page.py
@@ -0,0 +1,18 @@
+from fan_class import Fan
+
+# First Fan Object
+fan1 = Fan(
+    speed=Fan.FAST,
+    radius=10,
+    color="yellow",
+    on=True
+)
+
+# Second Fan Object
+fan2 = Fan(
+    speed=Fan.MEDIUM,
+    radius=5,
+    color="blue",
+    on=False
+)
+
